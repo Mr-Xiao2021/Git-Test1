f6ef355ed1342d96926e67a29004af163f5b2e03
Mr-Xiao2021 <94785565+Mr-Xiao2021@users.noreply.github.com>
2022-08-02 10:23:00 +0800

    pull-commit

--- src/main/java/com/xxr/inf/HelloGit.java
+++ src/main/java/com/xxr/inf/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git World!");
         System.out.println("push commit test");
+        System.out.println("pull commit test");
     }
 }
