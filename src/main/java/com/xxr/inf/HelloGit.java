da58a42e6f73f7695d03cf1c1cb98db433f270cb
Mr-Xiao2021 <3493602396@qq.com>
2022-08-02 09:40:25 +0800

    first-commit

---
+++ src/main/java/com/xxr/inf/HelloGit.java
@@ -0,0 +1,14 @@
+package com.xxr.inf;
+
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author Mr_X
+ * @Date 2022/8/2 9:39
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git World!");
+    }
+}
